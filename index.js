ef0ae4ecf839eb3674a985265705bf3cb9b661f1
Tanaka <74397585+tanakahk@users.noreply.github.com>
2021-07-23 01:58:35 -0300

    dia 1

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const dotenv = require('dotenv');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-dotenv.config();
-
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  // const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+  
+  
+  if (message.content === `${prefix}ping`) {
+    message.channel.send('Pong.');
+  } else if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`Vc n passou nenhum argumento, ${message.author}!`)
+    } else if (args[0] === 'foo') {
+      return message.channel.send('bar')
+    }
+    // message.channel.send(`Primeiro argumento: ${args[0]}`);
+    message.channel.send(`Argumentos: ${args}`)
+  }
+});
+
